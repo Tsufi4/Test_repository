831e3e793516c29f0e2e85656b99ff7d095afb2e
Jaroslav Coufal <cou41166@spseol.cz>
2024-01-19 09:01:31 +0100

    MUJ PRVNI COMMIT

---
+++ 1st_try.py
@@ -0,0 +1,37 @@
+# zamestnanec1 = ["petr","novak","olomouc",779 00, True]
+# zamestnanec2 = ["pavel","maly","prostejov",775 44, True]
+# zamestnanec3 = ["jan","velky","prerov", True]
+
+
+# print(zamestnanec1[1])
+
+
+zamestnanec1 = {"name":"jon","age": 30,"city":"NY"}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
